ad6d2651e3c6486fefa627ba8cbd9e03245553c8
medhini <medhini2004m@gmail.com>
2025-05-10 12:50:00 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const sgMail = require("@sendgrid/mail");
+const cors = require("cors");
+require("dotenv").config(); // Loads environment variables from .env
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+ // loaded from Render env
+
+app.post("/sendEmail", async (req, res) => {
+  const { name, email, message } = req.body;
+
+  const msg = {
+    to: "medhini.is22@bmsce.ac.in",
+    from: "medhini2004m@gmail.com", // must be verified in SendGrid
+    subject: `New Message from ${name}`,
+    text: `Email: ${email}\nMessage: ${message}`,
+  };
+
+  try {
+    await sgMail.send(msg);
+    res.status(200).send("Email sent");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error sending email");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
